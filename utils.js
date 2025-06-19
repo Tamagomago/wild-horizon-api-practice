ca3e464a35a48aa87ebb7c62deb4c34c957d1215
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-06-19 20:45:16 +0800

    initial commit

---
+++ utils.js
@@ -0,0 +1,11 @@
+export const setRes = (res, statusCode, body) => {
+	res.statusCode = statusCode;
+	res.setHeader('Content-Type', 'application/json');
+	res.end(body);
+};
+
+export const handleFilterByField = async (data, field, value) => {
+	return data.filter(
+		(item) => item[field].toLowerCase() === value.toLowerCase()
+	);
+};

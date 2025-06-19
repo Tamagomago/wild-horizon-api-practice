9722e3c153464220af3bb095286a135db00c7e49
Tamagomago <tamayoraeanchrissean@gmail.com>
2025-06-19 22:16:42 +0800

    feat(utils): add CORS headers to allow all origins and GET requests

--- utils.js
+++ utils.js
@@ -1,6 +1,8 @@
 export const setRes = (res, statusCode, body) => {
   res.statusCode = statusCode;
   res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.end(body);
 };
 
